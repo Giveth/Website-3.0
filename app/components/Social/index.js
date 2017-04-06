59e1173708dc98faab4b7845692701bcedebd742
Sébastien Rohan <rohan.sebastien@gmail.com>
2017-04-06 17:13:33 +0200

    Added spanish translation

--- app/components/Social/index.js
+++ app/components/Social/index.js
@@ -5,40 +5,49 @@ import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 import SocialListItem from './SocialListItem';
 
-function Social() {
-  const socialItem = [
-    { url: 'https://www.facebook.com/givethio/', type: 'facebook' },
-    { url: 'https://twitter.com/Givethio', type: 'twitter' },
-    { url: 'https://github.com/Giveth', type: 'github' },
-    { url: 'https://medium.com/giveth', type: 'medium' },
-    { url: 'http://slack.giveth.io/', type: 'slack' },
-  ];
-  return (
-    <div>
-      <Col xs={10} sm={8} xsOffset={1} smOffset={2}>
-        <center>
-          <h3>
-            <FormattedMessage {...messages.header} />
-          </h3>
-          <ul className="social" style={{ fontSize: '32px' }}>
-            { socialItem.map((item) => <SocialListItem key={item.type} url={item.url} type={item.type} />) }
-          </ul>
-        </center>
-      </Col>
-      <Row style={{ padding: '0 64px' }}>
-        <center>
-          <Col xs={6}>
-            <FBPage appId={'givethio'} href={'https://www.facebook.com/givethio'} width={484} height={600} tabs={['timeline', 'events', 'messages']} style={{ paddingRight: '16px' }} />
-          </Col>
-        </center>
-        <center>
-          <Col xs={6}>
-            <a className="twitter-timeline" href="https://twitter.com/Givethio">Tweets by Givethio</a> <script async src="//platform.twitter.com/widgets.js" charSet="utf-8"></script>
-          </Col>
-        </center>
-      </Row>
-    </div>
-  );
-}
+export default class Social extends React.PureComponent {
 
-export default Social;
+  componentWillMount() {
+    const script = document.createElement('script');
+    script.src = '//platform.twitter.com/widgets.js';
+    script.charSet = 'utf-8';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+
+  render() {
+    const socialItem = [
+      { url: 'https://www.facebook.com/givethio/', type: 'facebook' },
+      { url: 'https://twitter.com/Givethio', type: 'twitter' },
+      { url: 'https://github.com/Giveth', type: 'github' },
+      { url: 'https://medium.com/giveth', type: 'medium' },
+      { url: 'http://slack.giveth.io/', type: 'slack' },
+    ];
+    return (
+      <div>
+        <Col xs={10} sm={8} xsOffset={1} smOffset={2}>
+          <center>
+            <h3>
+              <FormattedMessage {...messages.header} />
+            </h3>
+            <ul className="social" style={{ fontSize: '32px' }}>
+              { socialItem.map((item) => <SocialListItem key={item.type} url={item.url} type={item.type} />) }
+            </ul>
+          </center>
+        </Col>
+        <Row style={{ padding: '0 64px' }}>
+          <center>
+            <Col xs={6}>
+              <FBPage appId={'givethio'} href={'https://www.facebook.com/givethio'} width={484} height={600} tabs={['timeline', 'events', 'messages']} style={{ paddingRight: '16px' }} />
+            </Col>
+          </center>
+          <center>
+            <Col xs={6}>
+              <a className="twitter-timeline" href="https://twitter.com/Givethio">Tweets by Givethio</a>
+            </Col>
+          </center>
+        </Row>
+      </div>
+    );
+  }
+}
--- app/i18n.js
+++ app/i18n.js
@@ -7,18 +7,22 @@
 import { addLocaleData } from 'react-intl';
 import enLocaleData from 'react-intl/locale-data/en';
 import frLocaleData from 'react-intl/locale-data/fr';
+import esLocaleData from 'react-intl/locale-data/es';
 
 import { DEFAULT_LOCALE } from './containers/App/constants'; // eslint-disable-line
 import enTranslationMessages from './translations/en.json';
 import frTranslationMessages from './translations/fr.json';
+import esTranslationMessages from './translations/es.json';
 
 export const appLocales = [
   'en',
   'fr',
+  'es',
 ];
 
 addLocaleData(enLocaleData);
 addLocaleData(frLocaleData);
+addLocaleData(esLocaleData);
 
 export const formatTranslationMessages = (locale, messages) => {
   const defaultFormattedMessages = locale !== DEFAULT_LOCALE
@@ -36,4 +40,5 @@ export const formatTranslationMessages = (locale, messages) => {
 export const translationMessages = {
   en: formatTranslationMessages('en', enTranslationMessages),
   fr: formatTranslationMessages('fr', frTranslationMessages),
+  es: formatTranslationMessages('es', esTranslationMessages),
 };
