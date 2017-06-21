c09c0626b14e0dc711fe1578a4515aff3b17b0bf
jennzo <jenatgiveth@gmail.com>
2017-06-21 18:04:29 +0100

    commit changes to fix typo

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -52,7 +52,7 @@ var Home = React.createClass({
 							<div className="col-xs-12 col-md-6 explanation-paragraph">
 								<div className="ep1">
 									<p>New technologies enable us to rethink and reengineer the distribution of charitable donations to lower transactions costs and so much more.</p>
-									<p>The traditional charitable system has undergone a slow evolution. It is out of date and out of touch. Too often it provides short-term, one-size-fits-all solutions that fail address the root causes of the problems they are trying to solve.</p>
+									<p>The traditional charitable system has undergone a slow evolution. It is out of date and out of touch. Too often it provides short-term, one-size-fits-all solutions that fail to address the root causes of the problems they are trying to solve.</p>
 								</div>
 							</div>
 						</div>
